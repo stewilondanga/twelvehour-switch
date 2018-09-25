b664f9e259f3a80ce6c791bb424a88a036bc1678
Stewart ilondanga <stilondanga@gmail.com>
2018-09-25 15:37:26 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ var navigate = (function() {
     $('.' + dataName).toggle();
   });
 })();
+
+var darkSide = new Audio();
 darkSide.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/480623/vadar_breathing.mp3";
 
 var lightSide = new Audio();
@@ -13,19 +15,19 @@ lightSide.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/480623/yoda_use_th
 
 var clickCount = 0;
 
-$('.toggle-container').on('click', function() {
+$('.toggle-container').on('click', function(){
   $('.toggle-handle').toggleClass('toggle-handle-day');
   $('body').toggleClass('off');
   $('.toggle-container').toggleClass('toggle-container-day');
   $('.star').toggleClass('star-day');
   $('.crater').toggleClass('crater-day');
   $('.mini-cloud').toggleClass('cloud-day');
-  if (clickCount === 0) {
+  if (clickCount === 0){
     clickCount++;
     darkSide.pause();
     lightSide.play();
     return darkSide.currentTime = 0;
-  } else if (clickCount === 1) {
+  } else if (clickCount === 1){
     clickCount--;
     lightSide.pause();
     darkSide.play();
